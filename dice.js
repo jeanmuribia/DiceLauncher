783c5944095361f26ec0c7e860801e473c12673b
jeanmuribia <115085099+jeanmuribia@users.noreply.github.com>
2024-03-30 10:57:20 +0100

    Avec le bouton réinitialiser

--- dice.js
+++ dice.js
@@ -10,9 +10,16 @@ function rollDice() {
     document.getElementById('results').innerText = `Résultats: ${results.join(', ')}`;
 
     calculateOutcome(results);
-    selectedDice = []; // Reset après le lancement
-    document.getElementById('selected-dice').innerText = `Dés sélectionnés:`;
+   
 }
+
+function resetDice() {
+    selectedDice = []; // Efface la sélection de dés
+    document.getElementById('selected-dice').innerText = 'Dés sélectionnés:';
+    document.getElementById('results').innerText = '';
+    document.getElementById('summary').innerText = '';
+}
+
 
 function calculateOutcome(results) {
     let successes = results.filter(result => result >= 7).length;
@@ -23,18 +30,20 @@ function calculateOutcome(results) {
 
     // Ajoute les pictogrammes pour les réussites
     for (let i = 0; i < successes; i++) {
-        let img = document.createElement('img');
-        img.src = 'dés/coche.png'; // Chemin vers l'image de réussite
-        img.alt = 'Réussite';
-        summaryElement.appendChild(img);
+        let imgSuccess = document.createElement('img');
+        imgSuccess.src = 'dés/coche.png'; // Chemin vers l'image de réussite
+        imgSuccess.alt = 'Réussite';
+        imgSuccess.className = 'success-icon';
+        summaryElement.appendChild(imgSuccess);
     }
 
     // Ajoute les pictogrammes pour les échecs
     for (let i = 0; i < failures; i++) {
-        let img = document.createElement('img');
-        img.src = 'dés/fermer.png'; // Chemin vers l'image d'échec
-        img.alt = 'Échec';
-        summaryElement.appendChild(img);
+        let imgFailure = document.createElement('img');
+        imgFailure.src = 'dés/fermer.png'; // Chemin vers l'image d'échec
+        imgFailure.alt = 'Échec';
+        imgFailure.className = 'failure-icon'
+        summaryElement.appendChild(imgFailure);
     }
 }
 
